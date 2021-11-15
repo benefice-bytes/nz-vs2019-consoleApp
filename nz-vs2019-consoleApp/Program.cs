ea26a4a96d8c9903fada699bbb1159a399167e3d
Nazeeruddin <naseers.tech@gmail.com>
2021-11-15 19:26:21 +0530

    Add project files.

---
+++ nz-vs2019-consoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nz_vs2019_consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
