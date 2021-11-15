3702345cfa5c6ad07bb2121328ec4700398ea775
Sazid <sazid.sunny@gmail.com>
2021-11-15 19:31:37 +0530

    C# Tutorials added

--- nz-vs2019-consoleApp/Program.cs
+++ nz-vs2019-consoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace nz_vs2019_consoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("C# Tutorials");
         }
     }
 }
